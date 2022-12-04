dc3568987379963f1a0c167992d1e65a952724d3
Shivam310 <71927997+Shivam310@users.noreply.github.com>
2022-12-04 15:46:47 +0530

    Add files via upload

---
+++ LoRa_Node_Ver-Beta-0.1.ino
@@ -0,0 +1,112 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+#include <LoRa.h>
+
+#define OLED_RESET 4
+Adafruit_SH1106 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
+
+#if (SH1106_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SH1106.h!");
+#endif
+
+int counter = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(5, INPUT);
+  while (!Serial);
+  Serial.println("LoRa Node");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  LoRa.setTxPower(20);
+
+}
+
+
+void loop() {
+
+  if (digitalRead(5) == HIGH) {
+    Serial.println("Button is pressed");
+    Serial.print("Sending packet: ");
+    Serial.println(counter);
+
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("hello ");
+    LoRa.print(counter);
+    LoRa.endPacket();
+    counter++;
+    delay(5000);
+    display.clearDisplay();
+    display.display();
+  }
+
+  else {
+
+    int packetSize = LoRa.parsePacket();
+
+    if (packetSize) {
+
+      // received a packet
+      Serial.print("Received packet '");
+      display.begin(SH1106_SWITCHCAPVCC, 0x3C);
+      display.clearDisplay();
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 0);
+      display.println("Emergency!");
+      display.display();
+      delay(2000);
+
+      // read packet
+      while (LoRa.available()) {
+        Serial.print((char)LoRa.read());
+      }
+      
+      // print RSSI of packet
+      Serial.print("' with RSSI ");
+      Serial.println(LoRa.packetRssi());
+      display.clearDisplay();
+      display.display();
+      
+      //Forward Emergency Packet Further(TBD- Forward Emergency Site location further)
+      LoRa.beginPacket();
+      LoRa.print("hello ");
+      LoRa.endPacket();
+    }
+
+  }
+
+}
